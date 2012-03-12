4eb4b3d24e2a37b450e572af1fad2aacb9c94723
Bruno Garcia <b@aduros.com>
2012-03-12 04:09:07 -0700

    Add Piratemark demo.

---
+++ demos/piratemark/src/Bouncy.hx
@@ -0,0 +1,54 @@
+import flambe.Component;
+import flambe.display.Sprite;
+import flambe.math.FMath;
+import flambe.System;
+
+class Bouncy extends Component
+{
+    public function new ()
+    {
+        _vr = FMath.toDegrees(Math.random()*0.2 - 0.1);
+        _vx = Math.random()*70 - 35;
+        _vy = Math.random()*70 - 35;
+    }
+
+    override public function onUpdate (dt :Int)
+    {
+        var sprite = owner.get(Sprite);
+        var w = System.stage.width;
+        var h = System.stage.height;
+
+        // Clamp to prevent warping when unpausing
+        if (dt > 100) {
+            dt = 100;
+        }
+
+        var perSecond = 0.05*dt;
+        sprite.x._ += _vx*perSecond;
+        sprite.y._ += _vy*perSecond;
+        sprite.rotation._ += _vr*perSecond;
+
+        _vy += 0.75*perSecond;
+
+        if (sprite.y._ > h) {
+            _vy *= -0.8;
+            sprite.y._ = h;
+            if (Math.random() < 0.5) {
+                _vy -= Math.random()*12;
+            }
+        }
+
+        if (sprite.x._ > w) {
+            _vx *= -0.8;
+            sprite.x._ = w;
+
+        } else if (sprite.x._ < 0) {
+            _vx *= -0.8;
+            sprite.x._ = 0;
+        }
+    }
+
+    private var _vx :Float;
+    private var _vy :Float;
+    private var _vr :Float;
+}
---
+++ demos/piratemark/src/Piratemark.hx
@@ -0,0 +1,56 @@
+import flambe.asset.AssetPack;
+import flambe.asset.Manifest;
+import flambe.debug.FpsDisplay;
+import flambe.display.FillSprite;
+import flambe.display.Font;
+import flambe.display.ImageSprite;
+import flambe.display.Sprite;
+import flambe.display.TextSprite;
+import flambe.Entity;
+import flambe.System;
+
+class Piratemark
+{
+    private static function main ()
+    {
+        // Wind up all platform-specific stuff
+        System.init();
+
+        // Load up the compiled pack in the assets directory named "bootstrap"
+        var manifest = Manifest.build("bootstrap");
+        var loader = System.loadAssetPack(manifest);
+        loader.get(onSuccess);
+    }
+
+    private static function onSuccess (pack :AssetPack)
+    {
+        var background = new Entity()
+            .add(new FillSprite(0xffffff, System.stage.width, System.stage.height));
+        System.root.addChild(background);
+
+        // Resize the background when the stage resizes
+        System.stage.resize.connect(function () {
+            var sprite = background.get(FillSprite);
+            sprite.width._ = System.stage.width;
+            sprite.height._ = System.stage.height;
+        });
+
+        // Add a bunch of pirates
+        for (ii in 0...10) {
+            var pirate = new Entity()
+                .add(new ImageSprite(pack.loadTexture("pirate.png")))
+                .add(new Bouncy());
+            var sprite = pirate.get(Sprite);
+            sprite.setXY(Math.random()*System.stage.width, Math.random()*System.stage.height);
+            sprite.setScale(Math.random() + 0.5);
+            sprite.centerAnchor();
+
+            System.root.addChild(pirate);
+        }
+
+        // Show an FPS display
+        System.root.addChild(new Entity()
+            .add(new TextSprite(new Font(pack, "tinyfont")))
+            .add(new FpsDisplay()));
+    }
+}
