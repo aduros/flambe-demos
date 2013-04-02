bd59e2cf4a2ee06b88e3f7857e69db43d61a9780
Bruno Garcia <b@aduros.com>
2013-04-02 05:06:24 -0700

    Go nuts with the browser testing.

--- selenium/driver.py
+++ selenium/driver.py
@@ -6,7 +6,9 @@
 import base64
 import httplib
 import json
+import new
 import os
+import random
 import sys
 import unittest
 
@@ -14,23 +16,22 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
 class FlambeTest(unittest.TestCase):
+    # Test classes are generated automagically further below
+    __test__ = False
+
     def setUp (self):
         self.username = "flambe"
         self.key = os.environ["SAUCE_ACCESS_KEY"]
 
-        # TODO(bruno): Run on all zillion browser platforms
-        opts = webdriver.DesiredCapabilities.IPHONE
-        opts["version"] = "5.0"
-        opts["platform"] = "MAC"
-        opts["name"] = "Flambe Selenium"
+        self.caps["name"] = "Flambe Selenium"
         if os.environ.get("TRAVIS"):
-            opts["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
-            opts["build"] = os.environ["TRAVIS_BUILD_NUMBER"]
-            opts["tags"] = [os.environ["TRAVIS_BRANCH"], "CI"]
+            self.caps["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+            self.caps["build"] = os.environ["TRAVIS_BUILD_NUMBER"]
+            self.caps["tags"] = [os.environ["TRAVIS_BRANCH"]]
 
         # localhost:4445 gets tunneled over Sauce Connect
         hub_url = "http://%s:%s@localhost:4445/wd/hub" % (self.username, self.key)
-        self.driver = webdriver.Remote(desired_capabilities=opts, command_executor=hub_url)
+        self.driver = webdriver.Remote(desired_capabilities=self.caps, command_executor=hub_url)
 
         self.job_id = self.driver.session_id
         print("Running job: https://saucelabs.com/jobs/%s" % self.job_id)
@@ -44,7 +45,7 @@ class FlambeTest(unittest.TestCase):
         # Poll the app for the test status variable
         status = [None] # An array because closure scope in Python is whack
         def check_status (driver):
-            status[0] = driver.execute_script("return $flambe_selenium_status")
+            status[0] = driver.execute_script("return window.$flambe_selenium_status")
             return status[0] != None
         WebDriverWait(self.driver, 30).until(check_status)
 
@@ -62,5 +63,36 @@ class FlambeTest(unittest.TestCase):
                            result, headers={"Authorization": "Basic %s" % auth})
         self.assertEquals(connection.getresponse().status, 200)
 
-if __name__ == "__main__":
-    unittest.main()
+PLATFORMS = [
+    dict(webdriver.DesiredCapabilities.CHROME, platform="WINDOWS"),
+    dict(webdriver.DesiredCapabilities.FIREFOX, platform="WINDOWS"),
+    dict(webdriver.DesiredCapabilities.INTERNETEXPLORER, version="10", platform="WINDOWS"),
+    dict(webdriver.DesiredCapabilities.INTERNETEXPLORER, version="9", platform="WINDOWS"),
+    dict(webdriver.DesiredCapabilities.OPERA, platform="WINDOWS"),
+
+    dict(webdriver.DesiredCapabilities.CHROME, platform="MAC"),
+    dict(webdriver.DesiredCapabilities.FIREFOX, platform="MAC"),
+    dict(webdriver.DesiredCapabilities.SAFARI, platform="MAC"),
+
+    dict(webdriver.DesiredCapabilities.CHROME, platform="LINUX"),
+    dict(webdriver.DesiredCapabilities.FIREFOX, platform="LINUX"),
+    dict(webdriver.DesiredCapabilities.OPERA, platform="LINUX"),
+
+    dict(webdriver.DesiredCapabilities.IPAD, version="6", platform="MAC"),
+    dict(webdriver.DesiredCapabilities.IPAD, version="5", platform="MAC"),
+    dict(webdriver.DesiredCapabilities.IPHONE, version="6", platform="MAC"),
+    dict(webdriver.DesiredCapabilities.IPHONE, version="5", platform="MAC"),
+    # dict(webdriver.DesiredCapabilities.ANDROID),
+]
+
+# Generate unit test classes for each platform
+classes = {}
+for platform in PLATFORMS:
+    d = dict(FlambeTest.__dict__)
+    name = "%s_%s_%s_%s" % (FlambeTest.__name__, platform["browserName"],
+        platform.get("platform", "ANY"), random.randint(0, 999))
+    name = name.replace(" ", "").replace(".", "")
+    d.update({"__test__": True, "caps": platform})
+    classes[name] = new.classobj(name, (FlambeTest,), d)
+
+globals().update(classes)
